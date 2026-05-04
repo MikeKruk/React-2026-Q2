9dafd0a889812622ec3e40be9fb2e0156c87d416
Mikhail Kruk <mi.kruk95@gmail.com>
2026-05-04 10:05:25 +0200

    feat: add localStoarage persistance for search term

--- rs-react-app/src/App.tsx
+++ rs-react-app/src/App.tsx
@@ -5,20 +5,23 @@ import CardList from './features/search/components/CardList';
 import SearchSection from './features/search/components/SearchSection';
 import Footer from './layout/Footer';
 import Header from './layout/Header';
+import { LOCAL_STORAGE_KEY } from './shared/constants/constants';
 import type { Pokemon, PokemonListItem } from './shared/types/types';
 
 interface State {
   pokemons: Pokemon[];
   currentTerm: string;
+  inputValue: string;
 }
 
 export default class App extends Component<object, State> {
   state: State = {
     pokemons: [],
     currentTerm: null,
+    inputValue: localStorage.getItem(LOCAL_STORAGE_KEY) || '',
   };
   async componentDidMount(): Promise<void> {
-    await this.fetchPokemons('');
+    await this.fetchPokemons(this.state.inputValue);
   }
 
   fetchPokemons = async (term: string) => {
@@ -42,15 +45,25 @@ export default class App extends Component<object, State> {
     }
   };
 
-  handelSearch = (term: string) => {
+  handelSearch = () => {
+    const term = this.state.inputValue.trim();
+    localStorage.setItem(LOCAL_STORAGE_KEY, term);
     this.fetchPokemons(term);
+  };
+
+  handelInputChange = (value: string) => {
+    this.setState({ inputValue: value });
   };
   render() {
     return (
       <div className="min-h-screen px-4 bg-background flex flex-col">
         <Header />
         <main className="flex-1 my-4 flex flex-col gap-8">
-          <SearchSection onSearch={this.handelSearch} />
+          <SearchSection
+            onSearch={this.handelSearch}
+            onChange={this.handelInputChange}
+            value={this.state.inputValue}
+          />
           <CardList pokemons={this.state.pokemons} />
         </main>
         <Footer />
--- rs-react-app/src/features/search/components/SearchSection.tsx
+++ rs-react-app/src/features/search/components/SearchSection.tsx
@@ -3,34 +3,18 @@ import SearchButton from './SearchButton';
 import SearchInput from './SearchInput';
 
 interface SearchSectionProps {
-  onSearch: (term: string) => void;
-}
-
-interface State {
   value: string;
+  onSearch: () => void;
+  onChange: (value: string) => void;
 }
 
-export default class SearchSection extends Component<
-  SearchSectionProps,
-  State
-> {
-  state: State = {
-    value: '',
-  };
-
-  handelChange = (value: string) => {
-    this.setState({ value });
-  }
-
-  handelSearch =( ) => {
-    const term = this.state.value.trim();
-    this.props.onSearch(term);
-  }
+export default class SearchSection extends Component<SearchSectionProps> {
   render() {
+    const { value, onSearch, onChange } = this.props;
     return (
       <section className="flex md:justify-center gap-8">
-        <SearchInput value={this.state.value} onChange={this.handelChange} />
-        <SearchButton onClick={this.handelSearch} />
+        <SearchInput value={value} onChange={onChange} />
+        <SearchButton onClick={onSearch} />
       </section>
     );
   }
