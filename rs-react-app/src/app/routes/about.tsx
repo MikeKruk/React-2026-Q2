3fd82de354c12f700dfaa5c797d31e50560a6687
Mikhail Kruk <mi.kruk95@gmail.com>
2026-05-19 00:47:21 +0200

    feat: add About page

--- rs-react-app/src/app/routes/about.tsx
+++ rs-react-app/src/app/routes/about.tsx
@@ -1,12 +1,9 @@
 import { createRoute } from '@tanstack/react-router';
+import AboutPage from '../../pages/AboutPage';
 import { Route as rootRoute } from './__root';
-
-function About() {
-  return <div>About</div>;
-}
 
 export const Route = createRoute({
   getParentRoute: () => rootRoute,
   path: '/about',
-  component: About,
+  component: AboutPage,
 });
--- rs-react-app/src/layout/Header.tsx
+++ rs-react-app/src/layout/Header.tsx
@@ -1,6 +1,13 @@
+import { Link, useMatchRoute } from '@tanstack/react-router';
+
 export default function Header() {
+  const useMatch = useMatchRoute();
+  const isHome = !!useMatch({
+    to: '/$page',
+    fuzzy: true,
+  });
   return (
-    <header className="flex justify-center">
+    <header className="flex justify-between items-center">
       <h1
         className="
       text-xl font-bold 
@@ -9,6 +16,22 @@ export default function Header() {
       >
         Pokémon Explorer
       </h1>
+      <nav className="flex gap-4">
+        <Link
+          to="/$page"
+          params={{ page: 1 }}
+          className={`hover:underline active:text-orange-500 ${isHome ? 'text-orange-500 font-bold' : ''}`}
+        >
+          Home
+        </Link>
+        <Link
+          to="/about"
+          className="hover:underline active:text-orange-500"
+          activeProps={{ className: 'text-orange-500 font-bold' }}
+        >
+          About
+        </Link>
+      </nav>
     </header>
   );
 }
