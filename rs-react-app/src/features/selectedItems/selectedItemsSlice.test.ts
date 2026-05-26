d6eef0a3b5f1f0343d11b668e237f84f35818258
Mikhail Kruk <mi.kruk95@gmail.com>
2026-05-26 02:08:27 +0200

    test: add tests for downloadCSV, selectedItemsSlice

---
+++ rs-react-app/src/features/selectedItems/selectedItemsSlice.test.ts
@@ -0,0 +1,27 @@
+import { mockPokemon } from '../../test-utils/mocks/mockPokemon';
+import reducer, {
+  selectItem,
+  unselectAllItems,
+  unselectItem,
+} from './selectedItemsSlice';
+
+describe('selectedItemsSlice', () => {
+  test('selectItem adds pokemon to state', () => {
+    const state = reducer(undefined, selectItem(mockPokemon));
+    expect(state.selectedItems).toHaveLength(1);
+    expect(state.selectedItems[0].id).toBe(mockPokemon.id);
+  });
+
+  test('unselectItem removes pokemon from state', () => {
+    const initial = reducer(undefined, selectItem(mockPokemon));
+    const state = reducer(initial, unselectItem(mockPokemon));
+    expect(state.selectedItems).toHaveLength(0);
+  });
+
+  test('unselectAllItems clears all selected items', () => {
+    let state = reducer(undefined, selectItem(mockPokemon));
+    state = reducer(state, selectItem(mockPokemon));
+    state = reducer(state, unselectAllItems());
+    expect(state.selectedItems).toHaveLength(0);
+  });
+});
